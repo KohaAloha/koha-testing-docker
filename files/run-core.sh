b1be4b11eb966edda131d7faf93015a4ffbe6c6a
Mason James <mtj@kohaaloha.com>
2020-11-28 00:35:34 +1300

    Sat Nov 28 00:35:34 NZDT 2020

--- files/run-core.sh
+++ files/run-core.sh
@@ -191,9 +191,8 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   SELENIUM_PORT=4444 \
                                   TEST_QA=1 \
                                   prove -j ${KOHA_PROVE_CPUS} -v \
-                                  --rules='par=t/db_dependent/00-strict.t' \
-                                  --rules='seq=t/db_dependent/**.t' --rules='par=**' \
-                                  --timer --harness=TAP::Harness::JUnit -s -r t/ xt/ \
+                                  --timer --harness=TAP::Harness::JUnit -s  \
+                                     t/00-testcritic.t \
                                   && touch testing.success"
     fi
 else
