ca44317bdf52487ba96b7ae7ef7a3b3410c873bb
Jonathan Druart <jonathan.druart@gmail.com>
2021-03-09 09:21:30 +0100

    Issue #245 - Correctly pass env vars for prove

--- files/run.sh
+++ files/run.sh
@@ -173,7 +173,13 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   mkdir cover_db; cp -r /cover_db/* cover_db;
                                   cover -report clover"
     elif [ "$LIGHT_TEST_SUITE" = "1" ]; then
-        koha-shell ${KOHA_INSTANCE} -p -c "JUNIT_OUTPUT_FILE=junit_main.xml \
+        koha-shell ${KOHA_INSTANCE} -p -c "find t xt -name '*.t' \
+                                    -not -path \"t/db_dependent/www/*\" \
+                                    -not -path \"t/db_dependent/selenium/*\" \
+                                    -not -path \"t/db_dependent/Koha/SearchEngine/Elasticsearch/*\" \
+                                    -not -path \"t/db_dependent/Koha/SearchEngine/*\" \
+                                |
+                                  JUNIT_OUTPUT_FILE=junit_main.xml \
                                   KOHA_TESTING=1 \
                                   KOHA_NO_TABLE_LOCKS=1 \
                                   KOHA_INTRANET_URL=http://koha:8081 \
@@ -181,12 +187,7 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   KOHA_USER=${KOHA_USER} \
                                   KOHA_PASS=${KOHA_PASS} \
                                   TEST_QA=1 \
-                                  find t xt -name '*.t' \
-                                    -not -path \"t/db_dependent/www/*\" \
-                                    -not -path \"t/db_dependent/selenium/*\" \
-                                    -not -path \"t/db_dependent/Koha/SearchEngine/Elasticsearch/*\" \
-                                    -not -path \"t/db_dependent/Koha/SearchEngine/*\" \
-                                | xargs prove -j ${KOHA_PROVE_CPUS} \
+                                  xargs prove -j ${KOHA_PROVE_CPUS} \
                                   --rules='par=t/db_dependent/00-strict.t' \
                                   --rules='seq=t/db_dependent/**.t' --rules='par=**' \
                                   --timer --harness=TAP::Harness::JUnit -s \
