db102307d098b6122da1b15d91670ba86189620e
Jonathan Druart <jonathan.druart@gmail.com>
2021-03-18 11:36:22 +0100

    Issue #247 - Embed Jenkins config build script
    We have one script per jobs defined in the Jenkins config.
    I am suggesting to embed the script in the ktd repo and modify the jenkins config to
    wget the script and execute it.
    
    It will prevent copy/paste error and to have to modify all the jobs when a change is needed.
    
    We can decide to move it out of the ktd repo if it does not belong here.

---
+++ jenkins_config/build.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+
+my $env_vars = {
+    RUN_TESTS_AND_EXIT => 'yes',
+    SYNC_REPO     => '.',
+    LOCAL_USER_ID => qx{id -u},
+    COVERAGE      => $ENV{COVERAGE},
+    KOHA_IMAGE    => $ENV{KOHA_IMAGE} || 'master',
+    KTD_BRANCH    => $ENV{KTD_BRANCH} || 'master',
+    LIGHT_RUN     => $ENV{LIGHT_RUN} // 1,
+    DBMS_YML      => $ENV{DBMS_YML} || '',
+};
+while ( my ( $var, $value ) = each %$env_vars ) {
+    $ENV{$var} = $value;
+}
+
+my @docker_compose_yml;
+
+# Cleanup
+run(q{rm -rf cover_db});
+run(q{git clean -f});
+
+my $GITLAB_RAW_URL = "https://gitlab.com/koha-community/koha-testing-docker/raw/" . $ENV{KTD_BRANCH};
+
+if ( $ENV{LIGHT_RUN} == 1 ) {
+    push @docker_compose_yml, 'docker-compose-light.yml';
+} else {
+    push @docker_compose_yml, 'docker-compose.yml';
+}
+
+if ( $ENV{DBMS_YML} ) {
+    push @docker_compose_yml, $ENV{DBMS_YML};
+}
+
+for my $yml ( @docker_compose_yml ) {
+    run(qq{wget -O $yml $GITLAB_RAW_URL/$yml}, 'or die');
+}
+
+my $docker_compose_env = "$GITLAB_RAW_URL/env/defaults.env";
+run(qq{wget -O .env $docker_compose_env}, 'or die');
+
+run(q{docker system prune -a -f});
+my $cmd = 'docker-compose ' . join( ' ', map { "-f $_" } @docker_compose_yml ) . ' pull';
+run($cmd, 'or die');
+
+# Run tests
+$cmd =
+    'docker-compose '
+  . join( ' ', map { "-f $_" } @docker_compose_yml )
+  . ' -p koha up --abort-on-container-exit --no-color --force-recreate';
+run($cmd, 'or die');
+
+# Post cleanup
+run(q{docker-compose down});
+run(qq{docker rm \$(docker ps -a -f "name=koha_" -q)});
+run(q{docker volume prune -f});
+run(q{docker image  prune -f});
+run(q{docker system prune -a -f});
+
+run(qq{rm $_}) for @docker_compose_yml;
+run(q{rm -rf .env});
+
+sub run {
+    my ( $cmd, $exit_on_error ) = @_;
+    if ( $exit_on_error ) {
+        say qx{$cmd} or die "Failed to execute $cmd";
+    } else {
+        say qx{$cmd};
+    }
+}
