7618b53d734aedebeb38ebe98613399e25d41785
Mason James <mtj@kohaaloha.com>
2021-04-02 02:10:32 +1300

    Fri Apr  2 02:10:32 NZDT 2021

--- files/run2.sh
+++ files/run2.sh
@@ -56,7 +56,13 @@ envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/bin/flush_memcached > ${BUILD_D
 # Make sure things are executable on /bin.
 chmod +x ${BUILD_DIR}/bin/*
 
+figlet koha-create
+
 koha-create --request-db ${KOHA_INSTANCE} --memcached-servers memcached:11211
+
+koha-list
+
+
 # Fix UID
 if [ ${LOCAL_USER_ID} ]; then
     usermod -u ${LOCAL_USER_ID} "${KOHA_INSTANCE}-koha"
