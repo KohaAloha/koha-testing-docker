a8b912b16a6fa6cf9e71f47a30b1e505c355ec44
Mason James <mtj@kohaaloha.com>
2021-04-07 02:58:05 +1200

    Wed Apr  7 02:58:05 NZST 2021

--- files/run.sh
+++ files/run.sh
@@ -339,22 +339,24 @@ pwd
 if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
     cd ${BUILD_DIR}/koha
     rm -rf /cover_db/*
-
-    koha-shell ${KOHA_INSTANCE} -p -c "pwd; \
-                                 JUNIT_OUTPUT_FILE=junit_main.xml \
+    koha-shell ${KOHA_INSTANCE} -p -c "find t xt -name '*.t' \
+                                    -not -path \"t/db_dependent/www/*\" \
+                                    -not -path \"t/db_dependent/selenium/*\" \
+                                    -not -path \"t/db_dependent/Koha/SearchEngine/Elasticsearch/*\" \
+                                    -not -path \"t/db_dependent/Koha/SearchEngine/*\" \
+                                |
+                                  JUNIT_OUTPUT_FILE=junit_main.xml \
                                   KOHA_TESTING=1 \
                                   KOHA_NO_TABLE_LOCKS=1 \
                                   KOHA_INTRANET_URL=http://koha:8081 \
                                   KOHA_OPAC_URL=http://koha:8080 \
                                   KOHA_USER=${KOHA_USER} \
                                   KOHA_PASS=${KOHA_PASS} \
-                                  SELENIUM_ADDR=selenium \
-                                  SELENIUM_PORT=4444 \
                                   TEST_QA=1 \
-                                  prove -j ${KOHA_PROVE_CPUS} -v \
+                                  xargs prove -j ${KOHA_PROVE_CPUS} \
                                   --rules='par=t/db_dependent/00-strict.t' \
                                   --rules='seq=t/db_dependent/**.t' --rules='par=**' \
-                                  --timer --harness=TAP::Harness::JUnit -r t/ xt/ \
+                                  --timer --harness=TAP::Harness::JUnit -s \
                                   && touch testing.success"
 else
   figlet end
