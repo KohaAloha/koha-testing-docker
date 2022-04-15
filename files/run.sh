3ee31df7860a6821a647167b5437e957f598c4e2
Mason James <mtj@kohaaloha.com>
2022-04-16 03:22:00 +1200

    add 'git config --global --add safe.directory /kohadevbox/koha'

--- files/run.sh
+++ files/run.sh
@@ -129,6 +129,7 @@ envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/instance_bashrc > /var/lib/koha
 
 # Configure git-bz
 cd /kohadevbox/koha
+git config --global --add safe.directory /kohadevbox/koha
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 git config bz.default-tracker bugs.koha-community.org
