2ee922e22c6eb1073cf1433806fa526407f06dae
Mason James <mtj@kohaaloha.com>
2022-05-21 02:34:27 +1200

    Sat May 21 02:34:27 NZST 2022

--- jenkins_config/build.pl
+++ jenkins_config/build.pl
@@ -24,7 +24,9 @@ my @docker_compose_yml;
 run(q{rm -rf cover_db});
 run(q{git clean -f});
 
-my $GITLAB_RAW_URL = "https://gitlab.com/koha-community/koha-testing-docker/raw/" . $ENV{KTD_BRANCH};
+#my $GITLAB_RAW_URL = "https://gitlab.com/koha-community/koha-testing-docker/raw/" . $ENV{KTD_BRANCH};
+
+my $GITLAB_RAW_URL = "https://gitlab.com/koha-community/mjames/raw/" . $ENV{KTD_BRANCH};
 
 if ( $ENV{LIGHT_RUN} == 1 ) {
     push @docker_compose_yml, 'docker-compose-light.yml';
