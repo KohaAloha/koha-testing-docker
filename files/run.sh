5931a3ade2057fd299d07bd7f63665c68aa9e056
Mason James <mtj@kohaaloha.com>
2022-07-05 05:46:39 +1200

    add LIGHT_RUN=2 to test elastic-search only

--- files/run.sh
+++ files/run.sh
@@ -214,12 +214,32 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   && touch testing.success; \
                                   mkdir cover_db; cp -r /cover_db/* cover_db;
                                   cover -report clover"
+
     elif [ "$LIGHT_TEST_SUITE" = "1" ]; then
         koha-shell ${KOHA_INSTANCE} -p -c "find t xt -name '*.t' \
                                     -not -path \"t/db_dependent/www/*\" \
                                     -not -path \"t/db_dependent/selenium/*\" \
                                     -not -path \"t/db_dependent/Koha/SearchEngine/Elasticsearch/*\" \
                                     -not -path \"t/db_dependent/Koha/SearchEngine/*\" \
+                                |
+                                  JUNIT_OUTPUT_FILE=junit_main.xml \
+                                  PERL5OPT=-MDevel::Cover=-db,/cover_db \
+                                  KOHA_TESTING=1 \
+                                  KOHA_NO_TABLE_LOCKS=1 \
+                                  KOHA_INTRANET_URL=http://koha:8081 \
+                                  KOHA_OPAC_URL=http://koha:8080 \
+                                  KOHA_USER=${KOHA_USER} \
+                                  KOHA_PASS=${KOHA_PASS} \
+                                  TEST_QA=1 \
+                                  xargs prove -j ${KOHA_PROVE_CPUS} \
+                                  --rules='par=t/db_dependent/00-strict.t' \
+                                  --rules='seq=t/db_dependent/**.t' --rules='par=**' \
+                                  --timer --harness=TAP::Harness::JUnit -r -s \
+                                  && touch testing.success"
+
+    elif [ "$LIGHT_TEST_SUITE" = "2" ]; then # test elastic-search only
+        koha-shell ${KOHA_INSTANCE} -p -c "find t -name '*.t' \
+                                    -path \"t/db_dependent/Koha/SearchEngine/*\" \
                                 |
                                   JUNIT_OUTPUT_FILE=junit_main.xml \
                                   KOHA_TESTING=1 \
