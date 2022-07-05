60fce9eca7a08fc156c5be883853054f72b89de3
Mason James <mtj@kohaaloha.com>
2022-07-05 18:44:15 +1200

    Issue #301: add --verbose to ES tests, for debugging

--- files/run.sh
+++ files/run.sh
@@ -248,7 +248,7 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   KOHA_USER=${KOHA_USER} \
                                   KOHA_PASS=${KOHA_PASS} \
                                   TEST_QA=1 \
-                                  prove --timer --harness=TAP::Harness::JUnit -r t/db_dependent/Koha/SearchEngine \
+                                  prove -v --timer --harness=TAP::Harness::JUnit -r t/db_dependent/Koha/SearchEngine \
                                   && touch testing.success"
     else
         koha-mysql ${KOHA_INSTANCE} -e "DROP DATABASE koha_${KOHA_INSTANCE};"
