1087abff66a1ec36d37379a83600e00c0de17603
Mason James <mtj@kohaaloha.com>
2022-07-05 23:25:48 +1200

    Issue #301: add more ES tests (3)

--- files/run.sh
+++ files/run.sh
@@ -249,10 +249,10 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
                                   KOHA_PASS=${KOHA_PASS} \
                                   TEST_QA=1 \
                                   prove -v --timer --harness=TAP::Harness::JUnit -r \
+                                    t/db_dependent/Search.t \
                                     t/Koha/Config.t \
                                     t/Koha/SearchEngine \
                                     t/db_dependent/Biblio.t \
-                                    t/db_dependent/Search.t \
                                     t/db_dependent/Koha/Authorities.t \
                                     t/db_dependent/Koha/Z3950Responder/GenericSession.t \
                                     t/db_dependent/Koha/SearchEngine \
