ec082b3b8c80e1e8c30468f2bfcc0f852009e51b
Your Name <you@example.com>
2022-07-07 16:51:33 +1200

    Signed-off-by: Your Name <you@example.com>
    	new file:   run-hello.sh

---
+++ run-hello.sh
@@ -0,0 +1,21 @@
+#docker system prune -a -f
+
+# Pre cleanup
+docker-compose down
+docker stop $(docker ps -a -f "name=koha_" -q)
+#docker rm $(docker ps -a -f "name=koha_" -q)
+#docker volume prune -f
+
+# Pull the latest image
+docker-compose -f docker-compose-hello.yml pull
+
+# Run tests
+#docker-compose -f docker-compose-hello.yml -p koha up --abort-on-container-exit --no-color --force-recreate
+docker-compose -f docker-compose-hello.yml -p koha up --no-color --force-recreate
+
+# Post cleanup
+docker-compose down
+docker stop $(docker ps -a -f "name=koha_" -q)
+#docker rm $(docker ps -a -f "name=koha_" -q)
+#docker volume prune -f
+
