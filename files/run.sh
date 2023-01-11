a490aff15b32ff35e20389a2c131ef66ed92adda
Tomas Cohen Arazi <tomascohen@theke.io>
2023-01-11 17:25:45 -0300

    Issue #335: Add the instance user to the sudoers (NOPASSW)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- files/run.sh
+++ files/run.sh
@@ -99,6 +99,7 @@ envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/vimrc                 > /root/.
 envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/bash_aliases          > /root/.bash_aliases
 envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/koha-conf-site.xml.in > /etc/koha/koha-conf-site.xml.in
 envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/koha-sites.conf       > /etc/koha/koha-sites.conf
+envsubst "$VARS_TO_SUB" < ${BUILD_DIR}/templates/sudoers               > /etc/sudoers.d/${KOHA_INSTANCE}
 # .gitconfig shouldn't get GIT_USER_* variables replaced
 cp ${BUILD_DIR}/templates/gitconfig /root/.gitconfig
 
